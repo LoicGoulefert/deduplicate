dec4ca294f80e34dd4264facd2bebc2411eceb29
Loic <loic.goulefert31@gmail.com>
2021-08-11 21:19:27 +0200

    numpy

--- deduplicate/deduplicate.py
+++ deduplicate/deduplicate.py
@@ -20,32 +20,42 @@ from tqdm import tqdm
     "-p", "--progress-bar", is_flag=True, default=True, help="Display a progress bar"
 )
 @vp.layer_processor
-def deduplicate(lines: LineCollection, tolerance: float, progress_bar: bool) -> LineCollection:
+def deduplicate(
+    lines: LineCollection,
+    tolerance: float,
+    progress_bar: bool,
+) -> vp.Document:
     """
     Remove duplicate lines.
 
     Args:
         lines: LineCollection input
         tolerance: maximum tolerance to consider 2 lines equal
-        progress_bar: display a progress bar if True
+        progress_bar: flag, display a progress bar if set
 
     Returns:
         a LineCollection where duplicated lines were removed.
     """
+
     lc = LineCollection()
     line_arr = np.array([np.array(line) for line in lines.as_mls()])
     mask = np.zeros(len(line_arr), dtype=bool)
 
     for i, line in enumerate(tqdm(line_arr[:-1], disable=progress_bar)):
-        reshaped = line.reshape(-1, 2, 2)
-        mask[i + 1 :] |= np.all(np.isclose(reshaped, line_arr[i + 1 :]), axis=(1, 2))
+        try:
+            reshaped = line.reshape(-1, 2, 2)
+        except ValueError:
+            raise ValueError("Use 'splitall' command before 'deduplicate'.")
+
         mask[i + 1 :] |= np.all(
-            np.isclose(reshaped[:, ::-1, :], line_arr[i + 1 :]), axis=(1, 2)
+            np.isclose(reshaped, line_arr[i + 1 :], atol=tolerance), axis=(1, 2)
+        )
+        mask[i + 1 :] |= np.all(
+            np.isclose(reshaped[:, ::-1, :], line_arr[i + 1 :], atol=tolerance),
+            axis=(1, 2),
         )
 
-    print("Len before =", len(line_arr))
     line_arr = line_arr[~mask]
-    print("Len after =", len(line_arr))
     lc.extend(MultiLineString(list(line_arr)))
 
     return lc
