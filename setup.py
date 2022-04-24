e82d14406770ade38aedd63bec986c7a0caa2603
L. Goulefert <loic.goulefert31@gmail.com>
2022-04-24 11:46:27 +0200

    Release setup (#6)
    * Clean setup.py
    
    * Add release workflow

--- setup.py
+++ setup.py
@@ -8,18 +8,27 @@ with open("LICENSE") as f:
 
 setup(
     name="deduplicate",
-    version="0.1.0",
-    description="",
+    version="0.1.0a1",
+    description="Duplicate line removal plug-in for vpype",
     long_description=readme,
     long_description_content_type="text/markdown",
     author="Loïc Goulefert",
-    url="",
+    url="https://github.com/LoicGoulefert/deduplicate",
     license=license,
     packages=["deduplicate"],
+    classifiers=[
+        "Development Status :: 4 - Beta",
+        "License :: OSI Approved :: MIT License",
+        "Topic :: Multimedia :: Graphics",
+        "Environment :: Plugins",
+    ],
+    setup_requires=["wheel"],
     install_requires=[
         "click",
+        "numpy",
+        "shapely>=1.8.0",
         "vpype>=1.9, <2.0",
-        "tqdm==4.61.1",
+        "tqdm>=4.61.1",
     ],
     entry_points="""
             [vpype.plugins]
